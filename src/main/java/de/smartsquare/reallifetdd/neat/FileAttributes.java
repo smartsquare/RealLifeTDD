d47cedaeb092abcaaaf7c18a3030114d41af4627
Daniel Rosowski <rosowski@smartsquare.de>
2014-01-14 17:25:44 +0100

    Added neat version of FileCleaner

---
+++ src/main/java/de/smartsquare/reallifetdd/neat/FileAttributes.java
@@ -0,0 +1,25 @@
+package de.smartsquare.reallifetdd.neat;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.LinkOption;
+import java.nio.file.attribute.FileTime;
+import java.util.Date;
+
+import de.smartsquare.reallifetdd.Calendar;
+
+public class FileAttributes {
+
+    private Calendar calendar;
+
+    public FileAttributes( Calendar calendar ) {
+        this.calendar = calendar;
+    }
+
+    public Date getLastModifiedTime( File file )
+        throws IOException {
+        FileTime fileTime = Files.getLastModifiedTime( file.toPath(), LinkOption.NOFOLLOW_LINKS );
+        return calendar.getDateFor( fileTime.toMillis() );
+    }
+}
--- src/main/java/de/smartsquare/reallifetdd/neat/FileCleaner.java
+++ src/main/java/de/smartsquare/reallifetdd/neat/FileCleaner.java
@@ -2,22 +2,20 @@ package de.smartsquare.reallifetdd.neat;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.LinkOption;
-import java.nio.file.attribute.FileTime;
-import java.util.Calendar;
 import java.util.Date;
 
 public class FileCleaner {
 
+    private FileAttributes fileAttributes;
+
+    public FileCleaner( FileAttributes fileAttributes ) {
+        this.fileAttributes = fileAttributes;
+    }
+
     public boolean cleanFileModifiedBeforeDate( File file, Date date )
         throws IOException {
-        FileTime fileTime = Files.getLastModifiedTime( file.toPath(), LinkOption.NOFOLLOW_LINKS );
-        Calendar calendar = Calendar.getInstance();
-        calendar.setTimeInMillis( fileTime.toMillis() );
-        Date modifiedDate = calendar.getTime();
-
-        if ( modifiedDate.after( date ) ) {
+        Date lastModifiedTime = fileAttributes.getLastModifiedTime( file );
+        if ( lastModifiedTime.before( date ) ) {
             return file.delete();
         }
 
